0ab84332635b476a7443fd321b1dbc56fd44f012
Jon Berry <jonpber@gmail.com>
2017-05-24 07:29:56 -0700

    fixed popping issue by using a hoverover load

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,9 @@ $(function() {
 		"simpsons", "animaniacs", "futurama", " family guy", "archer", "south park",
 		"cowboy bebop", "american dad", "bob's burgers"];
 
+		var movingGifHolder = new Image();
+		var stillGifHolder = new Image();
+
 		function buttonLoad(){
 			$(".buttonSlot").empty();
 			for (var i = 0; i < searchButtons.length; i++){
@@ -95,48 +98,25 @@ $(function() {
 	    		// buttonLoad();
 	    	}
 	    });
+		$(".gifHolder").on("mouseover", ".gifDiv", function(){
+			stillGifHolder.src = $(this).attr("data-stillUrl");
+			movingGifHolder.src = $(this).attr("data-movingGif");
+
+	    });
 
 	    $(".gifHolder").on("click", ".gifDiv", function(){
 	    	if ($(this).attr("data-playing") == 0){
 	    		$(this).html("<img src =" + $(this).attr("data-movingGif") + "><p>Rating: " + $(this).attr("data-rating") + "</p>");
-	    		$(this).attr("data-playing", 1);
+    			$(this).attr("data-playing", 1);
+
 	    	}
 
 	    	else {
-	    		$(this).html("<img src =" + $(this).attr("data-stillUrl") + "><p>Rating: " + $(this).attr("data-rating") + "</p>");
-	    		$(this).attr("data-playing", 0);
+	    		$(this).html("<img src=" + $(this).attr("data-stillUrl") + "><p>Rating: " + $(this).attr("data-rating") + "</p>");
+    			$(this).attr("data-playing", 0);
 	    	}
 	    });
 
-	 //    function preloadGifs(){
-	 //    	var gifs = $(".gifHolder").children();
-	 //    	for (i = 0; i < gifs.length; i++){
-	 //    		var preloadImg = $("<img src=" + $(gifs[i]).attr("data-movingGif") + ">");
-	 //    		var preloadImgStill = $("<img src=" + $(gifs[i]).attr("data-stillUrl") + ">");
-	 //    	}
-	 //    }
-
-		// $(window).resize(function() {
-		// 	if ($(this).width() >= 1200) {
-		// 	preloadGifs();
-		// 	}
-
-		// 	else if ($(this).width() < 1280 && $(this).width()>= 992) {
-		// 	preloadGifs();
-		// 	}
-
-		// 	else if ($(this).width() < 992 && $(this).width()>= 768){
-		// 	preloadGifs();
-		// 	}
-
-		// 	else if ($(this).width() < 768 && $(this).width()>= 568){
-		// 	preloadGifs();
-		// 	}
-
-		// 	else {
-		// 	preloadGifs();
-		// 	}
-		// });
 
 });
 
